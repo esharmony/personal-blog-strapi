e989a6460b080fcd1728a983e2a211558388935e
Matthew Barnden <matt.barnden@gmail.com>
2021-03-01 16:32:44 +0000

    added slugify

--- api/post/models/post.js
+++ api/post/models/post.js
@@ -1,8 +1,21 @@
 'use strict';
-
+const slugify = require('slugify');
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        beforeCreate: async (data) => {
+            if (data.Title) {
+            data.slug = slugify(data.Title);
+            }
+        },
+        beforeUpdate: async (params, data) => {
+            if (data.Title) {
+            data.slug = slugify(data.Title);
+            }
+        },
+    },
+};
