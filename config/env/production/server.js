f2303643a623355a6f039cd7f492a55c0da357d6
Matthew Barnden <development.esharmony@gmail.com>
2021-03-30 15:55:09 +0100

    running version

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,11 +1,10 @@
 module.exports = ({ env }) => ({
-    host: env('HOST'),
-    port: env.int('PORT'),
-    url: env('URL'),
-    admin: {
-      auth: {
-        secret: env('ADMIN_JWT_SECRET'),
-      },
+  host: env("HOST"),
+  port: env.int("PORT"),
+  url: env("URL"),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
     },
-  });
-  
+  },
+});
---
+++ config/middleware.js
@@ -0,0 +1,15 @@
+module.exports = {
+  //...
+  settings: {
+    cors: {
+      origin: [
+        "http://localhost",
+        "http://localhost:3000",
+        "https://www.sabbaticaldev.co.uk",
+        "https://sabbaticaldev.co.uk",
+        "https://easypie.co.uk",
+        "http://localhost:1337"
+      ],
+    },
+  },
+};
