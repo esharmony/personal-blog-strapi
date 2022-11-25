0141a8ecaae21d0c73a693c94dd4d81f158d43fd
Matthew Barnden <development.esharmony@gmail.com>
2022-11-25 11:35:52 +0000

    ready for render

--- config/middleware.js
+++ config/middleware.js
@@ -7,15 +7,15 @@ module.exports = {
         "http://localhost:3000",
         "https://www.sabbaticaldev.co.uk",
         "https://sabbaticaldev.co.uk",
-        "https://sabbatical-api.herokuapp.com",
-        "http://localhost:1337"
+        "https://sabattical-strapi.onrender.com",
+        "http://localhost:1337",
       ],
     },
     gzip: {
       enabled: true,
       options: {
-        br: false
-      }
-    }
+        br: false,
+      },
+    },
   },
 };
